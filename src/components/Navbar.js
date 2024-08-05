8ed23c7e6f74185ef04b36f24ac30676eb78625c
ndzamboni <ndzamboni@gmail.com>
2024-08-04 21:13:56 -0400

    some style changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,24 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-function Navbar() {
+function Navbar({ toggleDarkMode }) {
   const { user } = useSelector((state) => state.user);
 
   return (
     <nav className="nav">
-      <Link to="/">Home</Link>
-      {user ? (
-        <>
-          <Link to="/dashboard">Dashboard</Link>
-          <Link to="/stats">Stats</Link>
-          <button>Logout</button>
-        </>
-      ) : (
-        <>
-          <Link to="/register">Register</Link>
-          <Link to="/login">Login</Link>
-        </>
-      )}
+      <div>
+        <Link to="/">Home</Link>
+        {user ? (
+          <>
+            <Link to="/dashboard">Dashboard</Link>
+            <button>Logout</button>
+          </>
+        ) : (
+          <>
+            <Link to="/register">Register</Link>
+            <Link to="/login">Login</Link>
+          </>
+        )}
+      </div>
+      <button onClick={toggleDarkMode}>Toggle Dark Mode</button>
     </nav>
   );
 }
