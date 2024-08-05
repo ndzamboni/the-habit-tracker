316ab703ec63bbb0c93ee1733015140985e39bbe
ndzamboni <ndzamboni@gmail.com>
2024-08-05 19:05:46 -0400

    added sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../features/user/userSlice';
 import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import Sidebar from './Sidebar';
 
-function AppNavbar({ toggleDarkMode }) {
+function AppNavbar({ toggleDarkMode, darkMode }) {
   const { user } = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [showSidebar, setShowSidebar] = useState(false);
 
   const handleLogout = () => {
     dispatch(logout());
     navigate('/login');
   };
 
+  const handleSidebarClose = () => setShowSidebar(false);
+  const handleSidebarShow = () => setShowSidebar(true);
+
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand as={Link} to="/">Habit Tracker</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            {user ? (
-              <>
-                <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
-                <Button variant="outline-danger" onClick={handleLogout}>Logout</Button>
-              </>
-            ) : (
-              <>
-                <Nav.Link as={Link} to="/register">Register</Nav.Link>
-                <Nav.Link as={Link} to="/login">Login</Nav.Link>
-              </>
+    <>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand as={Link} to="/">Habit Tracker</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              {user ? (
+                <>
+                  <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
+                  <Button variant="outline-danger" onClick={handleLogout}>Logout</Button>
+                </>
+              ) : (
+                <>
+                  <Nav.Link as={Link} to="/register">Register</Nav.Link>
+                  <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                </>
+              )}
+            </Nav>
+            <Button variant="outline-secondary" onClick={toggleDarkMode}>Toggle Dark Mode</Button>
+            {user && (
+              <Button variant="primary" className="ms-2" onClick={handleSidebarShow}>
+                Customize
+              </Button>
             )}
-          </Nav>
-          <Button variant="outline-secondary" onClick={toggleDarkMode}>Toggle Dark Mode</Button>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Sidebar show={showSidebar} handleClose={handleSidebarClose} darkMode={darkMode} />
+    </>
   );
 }
 
