bb6f3c0a2b5283e87b1d9bf4cbdebe14289d3ebc
ndzamboni <ndzamboni@gmail.com>
2024-08-08 20:20:04 -0400

    bug with user profile

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { loadUser } from './features/user/userSlice';
 import Navbar from './components/Navbar';
@@ -7,8 +7,8 @@ import Home from './components/Home';
 import Register from './components/Register';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
+import UserProfile from './components/UserProfile';
 import './App.css';
-
 
 function App() {
   const dispatch = useDispatch();
@@ -19,17 +19,17 @@ function App() {
   }, [dispatch]);
 
   return (
-    <Router>
-      <div className={`app ${user ? (user.darkMode ? 'dark-mode' : 'light-mode') : 'light-mode'}`}>
-        <Navbar />
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/register" component={Register} />
-          <Route path="/login" component={Login} />
-          <Route path="/dashboard" component={Dashboard} />
-        </Switch>
-      </div>
-    </Router>
+    <div className={`app ${user ? (user.darkMode ? 'dark-mode' : 'light-mode') : 'light-mode'}`}>
+      <Navbar />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/user-profile" component={UserProfile} />
+        <Route path="*" component={() => '404 NOT FOUND'} />
+      </Switch>
+    </div>
   );
 }
 
